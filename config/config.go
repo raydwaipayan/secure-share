62c1e217f4c301fc52152de856b6b768588918cd
Dwaipayan Ray <dwaipayanray1@gmail.com>
2020-12-12 15:56:06 +0530

    conf: Load env variables through Viper
    Signed-off-by: Dwaipayan Ray <dwaipayanray1@gmail.com>

---
+++ config/config.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+// Config stores all configuration variables from environment
+type Config struct {
+	Port string
+}
+
+// Load the environment variables and return config struct
+func Load() Config {
+	viper.AutomaticEnv()
+
+	viper.BindEnv("Port", "PORT")
+	viper.SetDefault("Port", "8080")
+
+	var conf Config
+	err := viper.Unmarshal(&conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return conf
+}
